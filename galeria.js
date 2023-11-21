2e23ce7f70f1602c2798605b892b85284e554c9b
Guilhermeeduardokrein <137182386+Guilhermeeduardokrein@users.noreply.github.com>
2023-11-21 15:04:48 -0300

    Add files via upload

---
+++ galeria.js
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+<link rel="stylesheet" href="galeria.css">
+</head>
+
+<body>
+<div class="container">
+<div class="panel"
+style="background-image:url('https://images.app.goo.gl/UiAXGAAKbjChpjCc6')"&gt;
+<h3>livros</h3>
+</div>
+<div class="panel"
+style="background-image: url('https://images.app.goo.gl/42Z5Raq53CFw8jPg6')"&gt;
+</div>
+<div class="panel"
+style="background-image: url('https://images.app.goo.gl/BVqmhAC4rT8pe36X9')"&gt;
+<h3>livros</h3>
+</div>
+<div> class="panel"
+style="background-image: url('https://images.app.goo.gl/V3WcgY47pu9oyQCP9')"&gt;
+<h3>livros</h3>
+</div>
+<div class="panel"
+style="background-image: url('https://images.app.goo.gl/cDTTG2BujRanhNmx7')"&gt;
+<h3>livros</h3>
+</div>
+
+</div>
+
+<script src="galeria.js"></script>
+</body>
+
+</html>
